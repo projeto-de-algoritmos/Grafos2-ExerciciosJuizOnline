297418aa18db11603bf40617f7f047776329f21d
Gabriel Marcolino <gabrielmr356@hotmail.com>
2023-10-16 13:45:25 -0300

    Adicionando exercício 1764 do Beecrowd

---
+++ 1764/1764.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXSIZE 40050
+#define MAXSIZE_P 40000
+
+typedef struct {
+    int u, v, w;
+} edge;
+
+edge g[MAXSIZE];
+int p[MAXSIZE_P];
+
+int comparaArestas(edge *, edge *);
+int kruskal(int);
+int componente(int);
+
+int main() {
+    int quantidadeCidades, quantidadeCaminhos, i;
+
+    while (scanf("%d %d", &quantidadeCidades, &quantidadeCaminhos), quantidadeCidades && quantidadeCaminhos) {
+        memset(g, 0, sizeof(g));
+        memset(p, 0, sizeof(p));
+
+        for (i = 0; i < quantidadeCaminhos; ++i) {
+            scanf("%d %d %d", &g[i].v, &g[i].u, &g[i].w);
+        }
+        qsort(g, quantidadeCaminhos, sizeof(edge), comparaArestas);
+
+        for (i = 1; i <= quantidadeCidades; ++i) {
+            p[i] = i;
+        }
+
+        printf("%d\n", kruskal(quantidadeCaminhos));
+    }
+
+    return 0;
+}
+
+int comparaArestas(edge *a, edge *b) {
+    return a->w - b->w;
+}
+
+int componente(int i) {
+    if (i == p[i]) {
+        return i;
+    }
+
+    return componente(p[i]);
+}
+
+int kruskal(int quantidadeArestas) {
+    int i, ans, v, u;
+
+    for (i = 0, ans = 0; i < quantidadeArestas; ++i) {
+        v = componente(g[i].v);
+        u = componente(g[i].u);
+
+        if (v != u) {
+            p[v] = p[u], ans += g[i].w;
+        }
+    }
+
+    return ans;
+}
